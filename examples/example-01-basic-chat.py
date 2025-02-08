4285cce4eb0c0895e9c34540ccaae87769ad1c56
sbslee <sbstevenlee@gmail.com>
2025-02-08 15:42:34 +0900

    Update files

--- examples/example-01-basic-chat.py
+++ examples/example-01-basic-chat.py
@@ -11,6 +11,8 @@ if not st.session_state.submitted:
     with st.form("my_form"):
         st.session_state.openai_api_key = st.text_input("OpenAPI API Key", type="password")
         st.session_state.submitted = st.form_submit_button("Submit")
+        if st.session_state.submitted:
+            st.rerun()
 else:
     if "chat" not in st.session_state:
         st.session_state.chat = so.utils.BasicChat(
--- examples/example-02-assistant-chat-with-id.py
+++ examples/example-02-assistant-chat-with-id.py
@@ -11,6 +11,8 @@ if not st.session_state.submitted:
         st.session_state.openai_api_key = st.text_input("OpenAI API Key", type="password")
         st.session_state.assistant_id = st.text_input("OpenAI Assistant ID", type="password")
         st.session_state.submitted = st.form_submit_button("Submit")
+        if st.session_state.submitted:
+            st.rerun()
 else:
     if "chat" not in st.session_state:
         st.session_state.chat = so.utils.AssistantChat(
